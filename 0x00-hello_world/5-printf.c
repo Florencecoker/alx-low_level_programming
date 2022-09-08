c559ea6c922b784a73561a118b7c650a1475cbc8
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-08 20:42:11 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,5 @@
  *   * Return: 0
  *    */
 int main(void)
-{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+{printf("with proper grammar, but the outcome is a piece of art,\n");return (0);
 }
