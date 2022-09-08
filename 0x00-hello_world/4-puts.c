f3c6c2de9ec26e75a1ae1524a71163211490d427
Florencecoker <damselpiccolo18@gmail.com>
2022-09-08 13:26:23 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,21 @@
-#!/bin/bash
 #include <stdio.h>
+
+
+
 /**
- *  * main - main block
- *   * Return: 0
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
  *    */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");return (0);
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,23 @@
-#!/bin/bash
 #include <stdio.h>
+
+
+
 /**
- *  * main - entry point
- *   * Return: 0
- *    */
+ *
+ *  * main - prints the phrase with proper grammar,
+ *
+ *   * but the outcome is a piece of art,
+ *
+ *    * Return: 0 if exited properly, non-zero otherwise
+ *
+ *     */
+
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");return (0);
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
