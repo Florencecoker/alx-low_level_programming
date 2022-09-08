ddbafb545b1db484154888f7bf4d82b8939d2c6b
Florencecoker <damselpiccolo18@gmail.com>
2022-09-08 16:25:57 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,10 @@
 int main(void)
 {
 	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	
 	write(2,
+
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+
 	return (1);
 }
