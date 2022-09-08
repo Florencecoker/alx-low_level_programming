81a6ad4a544beabbfdca6e35e5169d579ef93365
Florencecoker <damselpiccolo18@gmail.com>
2022-09-08 12:14:29 -0700

    C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *  * main - entry point
+ *   * Return: 0
+ *    */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
