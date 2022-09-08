ed3394368c33e285c25dd2a305e21f628f5edf6a
Florencecoker <damselpiccolo18@gmail.com>
2022-09-08 16:36:14 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
-/**
- *	* main - main block
- *	* Return: 1
- */
-int main(void)
-{
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	
-	write(2,
 
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+/**
+ *
+ *  *main - prints the standrad error sandwiched around the  standard outpu/t
+ *
+ *   *Return:returns 1
+ *
+ *    **/
+
+int main(void)
+
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
+
 }
