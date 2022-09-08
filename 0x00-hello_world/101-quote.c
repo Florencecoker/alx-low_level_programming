e04d217e1b0537e7b2a643e124dc9aadf0ea033e
Florencecoker <damselpiccolo18@gmail.com>
2022-09-08 16:18:37 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,14 @@
-/*
-	* File: 101-quote.c
-	*  * Auth: Brennan D Baraban
-	*   */
-
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-
 /**
- *	* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *	* followed by a new line, to standard error.
- *	* Return: Always 1.
+ *	* main - main block
+ *	* Return: 1
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-
-			return (1);
+	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+	return (1);
 }
