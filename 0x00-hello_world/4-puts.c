c1cd882acf3700dad7aaa5d31ce9ab6931262fb2
Florencecoker <damselpiccolo18@gmail.com>
2022-09-08 13:36:29 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-/**
+/*
+ *   main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
+ *   Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 
