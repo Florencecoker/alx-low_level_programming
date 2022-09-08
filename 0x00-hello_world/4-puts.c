99dd8fe7c546fa03a4b2650aa5c06cdbecde98ec
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-08 20:39:44 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,5 @@
  *   * Return: 0
  *    */
 int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+{puts("\"Programming is like building a multilingual puzzle");return (0);
 }
