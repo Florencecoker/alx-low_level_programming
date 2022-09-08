87e98cfe640d2aa4c454f42bee8db24e29aa4449
Florencecoker <damselpiccolo18@gmail.com>
2022-09-08 14:52:29 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,20 +2,18 @@
 
 /**
  *
- *  * main - prints the phrase with proper grammar,
+ *	* main - prints the phrase with proper grammar,
  *
- *   * but the outcome is a piece of art,
+ *	* but the outcome is a piece of art,
  *
- *    * Return: 0 if exited properly, non-zero otherwise
+ *	* Return: 0 if exited properly, non-zero otherwise
  *
- *     */
+ */
 
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
