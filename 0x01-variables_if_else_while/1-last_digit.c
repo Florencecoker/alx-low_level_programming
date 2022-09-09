69d9c4537ba058e6d8780d5ec4b4da535816aa06
Florencecoker <damselpiccolo18@gmail.com>
2022-09-09 12:17:59 -0700

    C - Variables

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){ //Begining of your code
+
+	char zh = 'a';
+	while(zh <= 'z'){
+	putchar(zh);
+	++zh;			  
+}
+	putchar('\n');
+	
+	return (0);
+}
+ //End of your code
