1c9334c733122f8a06d18a5ff1f8821f0ddf9611
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-10 04:28:50 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d < 100; d++)
+	for (d = 1; d < 99; d++)
 	{
 		putchar((d / 10) + '0');
 		putchar((d % 10) + '0');
-		if (d != 99)
+		if (d != 89)
 		{
 			putchar(',');
 			putchar(' ');
