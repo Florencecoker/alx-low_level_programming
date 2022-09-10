bd246b07db5405a231ea13c647556b293659cee1
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-10 04:08:27 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,93 +1,47 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success/correct)
- *
- *     */
-
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
 
-
-
 {
-
-		int left;
-
-			int right;
-
-				int center;
+	int left;
+	int right;
+	int center;
 
 
 
+	for (left = 48; left <= 57; left++)
+	{
+		for (center = left + 1; center <= 57; center++)
+		{
+			for (right = center + 1 ; right <= 57; right++)
 
 
 
+			{
+				putchar(left);
+				putchar (center);
+				putchar (right);
 
-					for (left = 48; left <= 57; left++)
+				if  ((left == 55) && (center == left + 1) && (right == center + 1))
+				{
+					break;
+				}
 
-							{
+				putchar(',');
+				putchar (' ');
 
-										for (center = left + 1; center <= 57; center++)
+			}
 
-													{
+		}
 
-																	for (right = center + 1 ; right <= 57; right++)
+	}
 
+	putchar('\n');
 
-
-
-
-
-
-																					{
-
-																										putchar(left);
-
-																														putchar (center);
-
-																																		putchar (right);
-
-
-
-																																						if  ((left == 55) && (center == left + 1) && (right == center + 1))
-
-																																											{
-
-																																																	break;
-
-																																																					}
-
-
-
-																																										putchar(',');
-
-																																														putchar (' ');
-
-
-
-																																																	}
-
-
-
-																			}
-
-
-
-											}
-
-
-
-						putchar('\n');
-
-
-
-							return (0);
-
-
+	return (0);
 
 }
