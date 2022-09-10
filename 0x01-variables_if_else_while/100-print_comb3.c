67eae88729b702da6ec81af8dd46c40db0c403ad
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-10 04:09:38 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,49 +1,25 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success/completed)
- *
- *     */
-
-
+ * Return: Always 0 (Success/completed)
+ */
 
 int main(void)
-
 {
+	int d;
 
-		int d;
+	for (d = 0; d < 100; d++)
+	{
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-
-
-			for (d = 0; d < 100; d++)
-
-					{
-
-								putchar((d / 10) + '0');
-
-										putchar((d % 10) + '0');
-
-												if (d != 99)
-
-															{
-
-																			putchar(',');
-
-																						putchar(' ');
-
-																								}
-
-													}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
