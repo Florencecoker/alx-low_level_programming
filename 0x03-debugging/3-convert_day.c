d02afcb2a91847dc8895431ade68358fd66cf67c
Florencecoker <damselpiccolo18@gmail.com>
2022-09-15 10:28:37 -0700

    Debugging

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* Return: day of year
+*convert_day - converts day of month to day of year, without accounting
+*for leap year
+*@month: month in number format
+*@day: day of month
+*Return: day of year
 */
 
 int convert_day(int month, int day)
 {
 	switch (month)
-	{
+{
 	case 2:
 		day = 31 + day;
 		break;
@@ -47,6 +47,6 @@ int convert_day(int month, int day)
 		break;
 	default:
 		break;
-	}
+}
 	return (day);
 }
