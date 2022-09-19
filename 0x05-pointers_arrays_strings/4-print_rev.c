2aea7627981943059cc3f187e5ff3d2e5fe2fc2a
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-19 11:47:30 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,31 +1,26 @@
 #include "main.h"
 
-
-
 /**
-*print_rev - prints a string in reverse, followed by a new line
-*@s: string to be printed
-*/
-
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
 void print_rev(char *s)
-
 {
 	int i, j, len;
 
 	i = 0;
 
-
 	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-{
-i++;
-}
 	len = i;
 
 	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
 
-{
-											_putchar(s[j]);
-}
 	_putchar('\n');
 }
