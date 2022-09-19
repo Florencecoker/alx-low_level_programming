cffc4f8a5e0c776f6b45683d5d43f9d026db1e4f
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-19 11:56:57 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,55 +1,28 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * rev_string - reverses a string
- *
- *   * @s: string to be reversed
- *
- *    */
-
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
 void rev_string(char *s)
-
 {
+	char tmp;
+	int i, len, len1;
 
-		char tmp;
+	len = 0;
+	len1 = 0;
 
-			int i, len, len1;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
+	len1 = len - 1;
 
-
-				len = 0;
-
-					len1 = 0;
-
-
-
-						while (s[len] != '\0')
-
-								{
-
-											len++;
-
-												}
-
-
-
-							len1 = len - 1;
-
-
-
-								for (i = 0; i < len / 2; i++)
-
-										{
-
-													tmp = s[i];
-
-															s[i] = s[len1];
-
-																	s[len1--] = tmp;
-
-																		}
-
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
