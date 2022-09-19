e1acb44e0ad58184a5961589486cf1037ebe5abe
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-19 11:20:21 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,24 +1,11 @@
 #include "main.h"
 
-
-
 /**
-*swap_int - swaps the values of two integers
-*@a: pointer to first value
-*@b: pointer to second value
-*/
-
-void swap_int(int *a, int *b)
-
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
+ */
+void reset_to_98(int *n)
 {
-
-	int c;
-
-
-	c = *a;
-
-	*a = *b;
-
-	*b = c;
-
+	*n = 98;
 }
