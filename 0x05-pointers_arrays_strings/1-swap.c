2b7245837ad667af17b4a91f7463be8b4b2ab108
Florencecoker <damselpiccolo18@gmail.com>
2022-09-19 03:34:13 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,27 +3,21 @@
 
 
 /**
- *
- *  * swap_int - swaps the values of two integers
- *
- *   * @a: pointer to first value
- *
- *    * @b: pointer to second value
- *
- *     */
+*swap_int - swaps the values of two integers
+*@a: pointer to first value
+*@b: pointer to second value
+*/
 
 void swap_int(int *a, int *b)
 
 {
 
-		int c;
+	int c;
 
+	c = *a;
 
+	*a = *b;
 
-			c = *a;
-
-				*a = *b;
-
-					*b = c;
+	*b = c;
 
 }
