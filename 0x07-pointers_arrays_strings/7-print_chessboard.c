6e76f235a189489221d5bef1661196f6f758d958
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-26 19:58:25 +0100

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
-*print_chessboard - Entry point
-*@a: array
-*Return: Always 0 (Success)
-*/
-
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
+ */
 void print_chessboard(char (*a)[8])
 {
-	int i, n;
+	int i, j;
+
 	for (i = 0; i < 8; i++)
-{
-	for (n = 0; n < 8; n++)
-		_putchar(a[i][n]);
-	_putchar('\n');
-}
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
