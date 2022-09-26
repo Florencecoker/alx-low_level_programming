cb4b915a98ba9e616854bde0d2d04e7b0dd54611
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-26 19:45:32 +0100

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
-*_memcpy - Entry point
-*@dest: input
-*@src: input
-*@n: input
-*Return: Always 0 (Success)
-*/
-
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
+	}
+
 	return (dest);
 }
