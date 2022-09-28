64ea102058de1d5db9591a8f3f8b25c4df2c4f92
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-28 18:13:41 +0100

    0x08-recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,35 +1,18 @@
 #include "main.h"
 
-
-
 /**
- *
- * * factorial - youtube c tutorial: 5 - (5 - 1)
- *
- * * @n: input
- *
- * * Return: Always 0 (Success)
- *
- * */
-
-
+* factorial - youtube c tutorial: 5 - (5 - 1)
+* @n: input
+* Return: Always 0 (Success)
+*/
 
 int factorial(int n)
-
 {
 
-
-
-		if (n < 0)
-
-					return (-1);
-
-			else if (n == 0)
-
-						return (1);
-
-				else
-
-							return (n * factorial(n - 1));
-
-}
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}r
