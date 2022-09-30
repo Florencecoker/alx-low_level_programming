250f321d347ddf49daf8ab000981008c04d10e91
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-09-30 16:22:38 +0100

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
-* File: main.h
-* Auth: Brennan D Baraban
-* Desc: Header file containing the prototypes of all functions
-*       included in the static library libholberton.a.
-*/
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 int _islower(int c);
