bc233c06c3127de919e151aaa90a88c4c641441d
Florencecoker <damselpiccolo18@gmail.com>
2022-10-13 12:50:26 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,30 +19,30 @@ void print_all(const char * const format, ...)
 	va_start(ap, format);
 	j = 0;
 	while (format && format[j])
-	{
+{
 		space = "";
 		if (format[j + 1])
 			space = ", ";
 		switch (format[j])
-		{
+{
 
 		case 'c':
-			printf("%c%s", va_arg(ap, int), space);
-			break;
+	printf("%c%s", va_arg(ap, int), space);
+		break;
 		case 'i':
-			printf("%d%s", va_arg(ap, int), space);
-			break;
+	printf("%d%s", va_arg(ap, int), space);
+		break;
 		case 'f':
-			printf("%f%s", va_arg(ap, double), space);
-			break;
+	printf("%f%s", va_arg(ap, double), space);
+		break;
 		case 's':
-			str = va_arg(ap, char *);
-			if (!str || !*str)
-				str = "(nil)";
-			printf("%s%s", str, space);
-			break;
-		}
+	str = va_arg(ap, char *);
+	if (!str || !*str)
+	str = "(nil)";
+	printf("%s%s", str, space);
+	break;
+}
 		j++;
-	}
+}
 	printf("\n");
 }
