b6c93d8d0511b2e390d808de8adfa2f68a0dd231
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-10-25 18:40:33 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,89 +1,45 @@
 #include "lists.h"
 
-
-
 /**
- *
- * * insert_nodeint_at_index - Inserts a new node to a listint_t
- *
- * *                           list at a given position.
- *
- * * @head: A pointer to the address of the
- *
- * *        head of the listint_t list.
- *
- * * @idx: The index of the listint_t list where the new
- *
- * *       node should be added - indices start at 0.
- *
- * * @n: The integer for the new node to contain.
- *
- * *
- *
- * * Return: If the function fails - NULL.
- *
- * *         Otherwise - the address of the new node.
- *
- * */
-
+* insert_nodeint_at_index - Inserts a new node to a listint_t
+*                           list at a given position.
+* @head: A pointer to the address of the
+*        head of the listint_t list.
+* @idx: The index of the listint_t list where the new
+*       node should be added - indices start at 0.
+* @n: The integer for the new node to contain.
+*
+* Return: If the function fails - NULL.
+*         Otherwise - the address of the new node.
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-
 {
+	listint_t *new, *copy = *head;
+	unsigned int node;
 
-		listint_t *new, *copy = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-			unsigned int node;
+	new->n = n;
 
+	if (idx == 0)
+	{
+		new->next = copy;
+		*head = new;
+		return (new);
+	}
 
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
 
-				new = malloc(sizeof(listint_t));
+		copy = copy->next;
+	}
 
-					if (new == NULL)
+	new->next = copy->next;
+	copy->next = new;
 
-								return (NULL);
-
-
-
-						new->n = n;
-
-
-
-							if (idx == 0)
-
-									{
-
-												new->next = copy;
-
-														*head = new;
-
-																return (new);
-
-																	}
-
-
-
-								for (node = 0; node < (idx - 1); node++)
-
-										{
-
-													if (copy == NULL || copy->next == NULL)
-
-																	return (NULL);
-
-
-
-															copy = copy->next;
-
-																}
-
-
-
-									new->next = copy->next;
-
-										copy->next = new;
-
-
-
-											return (new);
-
+	return (new);
 }
