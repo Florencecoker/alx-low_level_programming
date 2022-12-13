245adeb7f1a9d12209941886c4c69ee4b2ca4a38
Florencecoker <damselpiccolo18@gmail.com>
2022-12-12 18:19:38 -0800

    task 0

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,21 +1,14 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _isupper - checks for uppercase character
- *
- *   * @c: the character to be checked
- *
- *    * Return: 1 if c is uppercase, 0 otherwise
- *
- *     */
+ *_isupper - checks for uppercase character
+ *@c: caracter to check
+ *Return: return 1 or 0
+ */
 
 int _isupper(int c)
-
 {
-
-		return (c >= 'A' && c <= 'Z');
-
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
