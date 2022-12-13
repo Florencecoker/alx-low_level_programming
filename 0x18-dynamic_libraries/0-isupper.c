dd895d9e8da8d531dc26c28032040a8fd373289b
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-12-13 01:43:06 +0100

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,21 +1,11 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _isupper - checks for uppercase character
- *
- *   * @c: the character to be checked
- *
- *    * Return: 1 if c is uppercase, 0 otherwise
- *
- *     */
-
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
-
 {
-
-		return (c >= 'A' && c <= 'Z');
-
+	return (c >= 'A' && c <= 'Z');
 }
