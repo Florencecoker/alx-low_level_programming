467a83c2b253fd560bdb09261b507accda68b1ca
Florence Coker Damilola <damselpiccolo18@gmail.com>
2023-01-07 03:48:34 +0100

    all done

--- 0x1A-hash_tables/Tests/0-main.c
+++ 0x1A-hash_tables/Tests/0-main.c
@@ -1,29 +1,16 @@
-nclude <stdlib.h>
-
+#include <stdlib.h>
 #include <string.h>
-
 #include <stdio.h>
-
 #include "../hash_tables.h"
 
-
-
 /**
+ * main - check the code for ALX-Africa Cohort 5 Students.
  *
- *  * main - check the code for ALX-Africa Cohort 5 Students.
- *
- *   *
- *
- *    * Return: Always EXIT_SUCCESS.
- *
- *     */
-
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
-
 {
-
-		hash_table_t *ht;
-
+	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
 	printf("%p\n", (void *)ht);
